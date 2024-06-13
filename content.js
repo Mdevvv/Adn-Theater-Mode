8b66e3a83998f309cc363162ed3af3e8799faf52
SEPT Mg <www.mg1.fr@gmail.com>
2024-06-13 18:23:58 +0200

    maj 1.1

--- content.js
+++ content.js
@@ -1,33 +1,80 @@
 var keyName = "Adn-Theater-Mode";
 
+var styleTagId = "customStylesAdnTheaterMode";
+
+// Function to detect URL changes
+function onUrlChange() {
+    if(window.location.href.startsWith("https://animationdigitalnetwork.fr/video/")  && localStorage.getItem(keyName) == "true") {
+        console.log("URL changed to:", window.location.href);
+        changeCSS();
+    }
+    else {
+        backCSS();
+    }
+    
+}
+
+// Initial URL
+let currentUrl = window.location.href;
+
+// Create a mutation observer to detect changes in the <title> element
+const observer = new MutationObserver(mutations => {
+mutations.forEach(mutation => {
+    if (currentUrl !== window.location.href) {
+    currentUrl = window.location.href;
+    onUrlChange();
+    }
+});
+});
+
+// Start observing the document title
+observer.observe(document, {
+childList: true,
+subtree: true
+});
+
+
 if(localStorage.getItem(keyName) === null) {
     localStorage.setItem(keyName, false);
 }
 
-
 if(localStorage.getItem(keyName) == "true") {
-    var styleTag = document.createElement('style');
-    
-    styleTag.innerHTML = ".lhIdsr { position: absolute !important; }";
-    styleTag.innerHTML += ".blomsM { width: 140% !important; }";
-    styleTag.innerHTML += ".eBWHrE { width: 90% !important; }";
-    styleTag.innerHTML += ".gYcEtV { width: 120% !important; }";
-
-    document.head.appendChild(styleTag);
+    changeCSS();
 }
-console.log("CONTENT");
+
+function changeCSS() {
+    
+        var styleTag = document.createElement('style');
+        
+        styleTag.id = styleTagId;
+
+        styleTag.innerHTML = ".lhIdsr { position: absolute !important; }";
+        styleTag.innerHTML += ".blomsM { width: 140% !important; }";
+        styleTag.innerHTML += ".eBWHrE { width: 90% !important; }";
+        styleTag.innerHTML += ".gYcEtV { width: 120% !important; }";
+    
+        document.head.appendChild(styleTag);
+}
+
+function backCSS() {
+    var styleTag = document.getElementById(styleTagId);
+    if (styleTag) {
+        styleTag.parentNode.removeChild(styleTag);
+    }
+}
+
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
+    //   console.log(sender.tab ?
+    //               "from a content script:" + sender.tab.url :
+    //               "from the extension");
       if (request.greeting === "hello")
         sendResponse({farewell: localStorage.getItem(keyName)});
       else if (request.greeting === "change") {
         localStorage.setItem(keyName, localStorage.getItem(keyName) == "true" ? "false" : "true");
         sendResponse({farewell: "changed"});
-        location.reload();
+        onUrlChange();
       }
 
       else if (request.greeting === "location")
--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 
 (async () => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, {greeting: "location"});
-    if(response.farewell.startsWith("https://animationdigitalnetwork.fr/video/")) {
-        document.getElementById("lds-ring").style.display = "none";    
-        document.getElementById("switch").style.display = "block";
-        
-        (async () => {
-            const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-            const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-            document.getElementById("checkbox").checked = response.farewell == "true";
-          })();
-        
-        document.getElementById("checkbox").addEventListener("change", function() {
+    try {
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        const response = await chrome.tabs.sendMessage(tab.id, {greeting: "location"});
+        if(response.farewell.startsWith("https://animationdigitalnetwork.fr/")) {
+            document.getElementById("lds-ring").style.display = "none";    
+            document.getElementById("switch").style.display = "block";
+            
             (async () => {
                 const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-                const response = await chrome.tabs.sendMessage(tab.id, {greeting: "change"});
-              })();
-        })
+                const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+                document.getElementById("checkbox").checked = response.farewell == "true";
+            })();
+            
+            document.getElementById("checkbox").addEventListener("change", function() {
+                (async () => {
+                    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+                    const response = await chrome.tabs.sendMessage(tab.id, {greeting: "change"});
+                })();
+            })
+        }
+    }
+    catch(err) {
+        document.getElementById("errMsg").style.display = "block";
+        document.getElementById("lds-ring").style.display = "none";
     }
 })();
